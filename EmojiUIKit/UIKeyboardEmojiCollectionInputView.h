7581fa4570f919ee77129ded3d8de11a80d75f05
PoomSmart <thatchapon.unp@outlook.com>
2021-02-27 10:17:43 +0700

    -

--- EmojiUIKit/UIKeyboardEmojiCollectionInputView.h
+++ EmojiUIKit/UIKeyboardEmojiCollectionInputView.h
@@ -10,4 +10,5 @@
 - (NSString *)emojiBaseUnicodeString:(NSString *)string; // iOS 8.3-9.3
 - (NSString *)emojiBaseString:(NSString *)string; // iOS >= 10
 - (NSString *)emojiBaseFirstCharacterString:(NSString *)string; // iOS >= 10
+- (BOOL)pointInside:(CGPoint)point forEvent:(void *)event;
 @end
