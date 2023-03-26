a6b962f2f0b41083d2b156ab2efc3db356cac574
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2023-03-26 10:07:01 +0700

    no vendor

--- buildLib.sh
+++ buildLib.sh
@@ -9,5 +9,5 @@ make clean
 make DEBUG=0 SIMULATOR=1
 cp .theos/obj/iphone_simulator/libEmojiLibrary.dylib libEmojiLibrary_pc.dylib
 lipo -create libEmojiLibrary_arm.dylib libEmojiLibrary_pc.dylib -output libEmojiLibrary.dylib
-mv libEmojiLibrary.dylib ${THEOS}/vendor/lib/
+mv libEmojiLibrary.dylib ${THEOS}/lib/
 rm -f libEmojiLibrary_arm.dylib libEmojiLibrary_pc.dylib
